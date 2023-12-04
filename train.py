e5f60c4730625e7e75118f4e8951edc492820e4f
TIENTI <tglukhikh@gmail.com>
2023-12-04 05:03:55 +0300

    working code. using mrpc task (Microsoft Research Paraphrase Corpus)

--- train.py
+++ train.py
@@ -1,19 +1,20 @@
+from transformers import BertTokenizer
 import torch.nn as nn
 import torch.optim as optim
 from model import MyModel
 from utils import load_data, smooth_decrease_weights
-from transformers import BertTokenizer
 
-# Parameters for quick testing
+model_name = 'bert-base-uncased'
+task_name = 'mrpc'  # Example GLUE task
 max_length = 128
 batch_size = 16  # Smaller batch size
 learning_rate = 1e-4
-num_epochs = 1  # Only one epoch for testing
+num_epochs = 5  # Only one epoch for testing
 decrease_factor = 0.95  # Adjust this factor for the weight decrease
 
 # Load a smaller subset of data for quick testing
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-train_loader = load_data(tokenizer, max_length, batch_size, subset_size=1000)  # Load only a subset
+train_loader = load_data(task_name, model_name, max_length, batch_size)
 
 # Initialize the model
 model = MyModel()  # Replace with your model's class name if different
@@ -25,9 +26,7 @@ optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
 # Training loop
 for epoch in range(num_epochs):
-    for i, batch in enumerate(train_loader):
-        if i >= 10:  # Run only a few batches for testing
-            break
+    for batch in train_loader:
         input_ids = batch['input_ids']
         attention_mask = batch['attention_mask']
         labels = batch['labels']
--- utils.py
+++ utils.py
@@ -1,8 +1,9 @@
 import torch
 from torch.utils.data import DataLoader, Dataset
+from transformers import BertTokenizer
 from datasets import load_dataset
-
-
+from torch.utils.data import DataLoader
+from transformers import DataCollatorWithPadding
 def smooth_decrease_weights(model, decrease_factor=0.99):
     """
     Gradually decreases the weights of the low-rank matrices in the ReLoRa layers of the model.
@@ -48,18 +49,35 @@ class TextDataset(Dataset):
             'labels': torch.tensor(label, dtype=torch.long)
         }
 
-def load_data(tokenizer, max_length, batch_size):
-    # Load IMDb dataset
-    dataset = load_dataset("imdb")
-    train_data = dataset['train']
 
-    # Create a Dataset
-    train_dataset = TextDataset(train_data, tokenizer, max_length)
+
+def load_data(task_name, model_name, max_length, batch_size):
+    # Load tokenizer
+    tokenizer = BertTokenizer.from_pretrained(model_name)
+
+    # Load dataset
+    dataset = load_dataset('glue', task_name)
+    train_dataset = dataset['train']
+
+    # Tokenize and align the labels with the tokenized inputs
+    def tokenize_function(examples):
+        return tokenizer(examples['sentence1'], examples['sentence2'], truncation=True, padding='max_length', max_length=max_length)
+
+    tokenized_datasets = train_dataset.map(tokenize_function, batched=True)
+    tokenized_datasets = tokenized_datasets.remove_columns(['sentence1', 'sentence2', 'idx'])
+    tokenized_datasets.set_format('torch')
+
+    # Data collator will dynamically pad the inputs received
+    data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
 
     # Create a DataLoader
-    data_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    data_loader = DataLoader(tokenized_datasets, shuffle=True, batch_size=batch_size, collate_fn=data_collator)
 
     return data_loader
+
+# Example usage
+# train_loader = load_data('mrpc', 'bert-base-uncased', max_length=128, batch_size=32)
+
 
 # Example usage
 # tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
